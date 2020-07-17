790b2ba28ea318ed5be13c5bd479ddfbdaf0f981
Michiel Keijser <63105573+Michielkeijser@users.noreply.github.com>
2020-07-17 13:16:31 +0200

    Module 3 opdracht 1

---
+++ Module-3/Eerste-JavaScript/script.js
@@ -0,0 +1,15 @@
+console.log('Hello Winc Acadamy');
+
+let firstName = 'Michiel'
+console.log(firstName);
+
+console.log(4 + 4);
+
+console.log("4" + "4");
+
+console.log(6 - 4);
+
+console.log(20 / 4);
+
+let age = "30";
+console.log(typeof(age));
