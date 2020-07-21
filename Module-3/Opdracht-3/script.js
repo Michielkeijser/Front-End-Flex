adb94ab4328f887ff81c7e2ad7749d53bfd7cbd6
Michiel Keijser <63105573+Michielkeijser@users.noreply.github.com>
2020-07-21 12:03:46 +0200

    Module 3 opdracht 3

---
+++ Module-3/Opdracht-3/script.js
@@ -0,0 +1,23 @@
+let age = 32;
+
+if (age > 18) {
+  console.log("Older then 18");
+} else {
+  console.log("Younger then 18");
+}
+
+let isFemale = true
+
+if (isFemale) {
+  console.log("Is female");
+} else {
+  console.log("Is not female");
+}
+
+let driverStatus = "bob"
+
+if (driverStatus == "bob") {
+  console.log("Has a driver");
+} else {
+  console.log("Does not have a driver");
+}
