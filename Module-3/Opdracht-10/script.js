fddfaed44ca5534e28dbb6be65a018aee45e3486
Michiel Keijser <63105573+Michielkeijser@users.noreply.github.com>
2020-07-29 15:14:46 +0200

    opdracht 10

---
+++ Module-3/Opdracht-10/script.js
@@ -0,0 +1,13 @@
+let guess = prompt('pick a number between 0 and 25');
+let answer = Math.floor(Math.random() * Math.floor(26));
+
+
+
+for (let i = 0; i < 10; i++) {
+  if (answer == guess) {
+    alert('You guessed the number!')
+    break
+  } else {
+    guess = prompt('Try again')
+  }
+}
