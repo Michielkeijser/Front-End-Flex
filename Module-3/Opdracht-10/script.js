0b491cfc9ee8a73b1b5edb17a0436a0161c3228b
Michiel Keijser <63105573+Michielkeijser@users.noreply.github.com>
2020-07-31 10:23:13 +0200

    opdracht 12

--- Module-3/Opdracht-10/script.js
+++ Module-3/Opdracht-10/script.js
@@ -1,8 +1,6 @@
 let guess = prompt('pick a number between 0 and 25');
 let answer = Math.floor(Math.random() * Math.floor(26));
 
-
-
 for (let i = 0; i < 10; i++) {
   if (answer == guess) {
     alert('You guessed the number!')
---
+++ Module-3/Opdracht-12/script.js
@@ -0,0 +1,11 @@
+const buttonAlert = document.getElementById('mybutton')
+const buttonColor = document.getElementById('backgroundcolor')
+const bodyElement = document.getElementById('body__opdracht')
+
+buttonAlert.addEventListener('click', function() {
+  alert('Button clicked!')
+})
+
+buttonColor.addEventListener('click', function() {
+  bodyElement.classList.toggle ('red-background')
+})
