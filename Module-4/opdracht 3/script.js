2e8641536373b4b2eac13c68f89c21b28b26d473
Michiel Keijser <chielkeijser@gmail.com>
2020-08-07 15:22:28 +0200

    Opdracht arrow functions

--- Module-4/opdracht 3/script.js
+++ Module-4/opdracht 3/script.js
@@ -1,4 +1,27 @@
-// zet de functie om naar een arrow functie
-const ikRockArrowFunctions = () => console.log('Joe, ik rock de arrow functions!')
+// Deel A
+const ikRockArrowFunctions = () =>
+  console.log("Joe, ik rock de arrow functions!");
 
-ikRockArrowFunctions()
+ikRockArrowFunctions();
+
+// Deel B
+const fivePlusSeven = () => 5 + 7;
+
+fivePlusSeven();
+
+// // Deel C
+// () => console.log(1 + 2);
+
+// Deel D
+
+myFunction = (a, b) => a + b;
+
+// Deel E
+
+addFive = (a) => a + 5;
+
+// Deel F
+
+createObject = () => (greeting: hoi);
+
+createObject();
