7d18c6e6b56d94c4862f90104aa9997eccc0322b
Michiel Keijser <chielkeijser@gmail.com>
2020-08-25 13:04:20 +0200

    Module-6 Opdracht-1

--- Module-6/Opdracht-1/functions.test.js
+++ Module-6/Opdracht-1/functions.test.js
@@ -11,9 +11,16 @@ test("Should be null", () => {
   expect(functions.isNull()).toBeNull();
 });
 
-test("checkValue Should be falsy when argument is undefined", () => {});
+test("checkValue Should be falsy when argument is undefined", () => {
+  expect(functions.checkValue()).toBeUndefined();
+});
 
-test("User should be Brad Traversy object", () => {});
+test("User should be Brad Traversy object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Brad",
+    lastName: "Traversy",
+  });
+});
 
 // Tests zijn eigenlijk gewoon functies. Om dat te laten zien laten we je
 // nu tests maken die werken zonder input van buitenaf:
@@ -23,15 +30,17 @@ test("Should be under or equal to 1600", () => {
   const load1 = 800;
   const load2 = 800;
   // expect(load1 + load2).toBe.....
+  expect(load1 + load2).toBe(1600);
 });
 
 // Regex
 test("There is no I in team", () => {
+  expect("team").not.toEqual("I");
   //  expect('team').not.to..........;
 });
 
 // Arrays
 test("Admin should be in usernames", () => {
   usernames = ["john", "karen", "admin"];
-  // expect(usernames).to......
+  expect(usernames).toContain("admin");
 });
