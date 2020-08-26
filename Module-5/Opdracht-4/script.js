3bebfab909ebb71b63d7ee7647a7253eb0e57d88
Michiel Keijser <chielkeijser@gmail.com>
2020-08-26 16:15:35 +0200

    Opdracht-4 Module-5

---
+++ Module-5/Opdracht-4/script.js
@@ -0,0 +1,16 @@
+const optellen = function (...args) {
+  return args.reduce((acc, item) => {
+    return acc + item;
+  });
+};
+
+console.log(optellen(1, 2, 3, 4, 5, 6));
+
+const optellenSpread = function (num1, num2, num3) {
+  return num1 + num2 + num3;
+};
+
+const cijfers = [1, 2, 3];
+
+console.log(optellenSpread(...cijfers));
+// verwachte uitkomst: 6
