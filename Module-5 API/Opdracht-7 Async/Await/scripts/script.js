3c83ba3dff63c02a98f8d28e80c11832168fc58e
Michiel Keijser <chielkeijser@gmail.com>
2020-09-01 15:14:21 +0200

    Opdracht-7-Module-5

--- Module-5 API/Opdracht-7 Async/Await/scripts/script.js
+++ Module-5 API/Opdracht-7 Async/Await/scripts/script.js
@@ -1,6 +1,47 @@
-const getDataCall = async function () {
-  const data = await getData();
-  console.log(data);
+const getMoviesByGenre = async function () {
+  let list = ``;
+  const listItem = document.querySelector(".genre__display");
+  const data = await getMovieGenres();
+  data.genres.forEach(item => {
+    list += `<li class="genres__item"> Genre name: ${item.name}, Id: ${item.id}  </li> `;
+    listItem.innerHTML = list;
+  });
 };
 
-getDataCall();
+getMoviesByGenre();
+
+const getMoviesById = async function () {
+  let list = ``;
+  const listItem = document.querySelector(".top__movie");
+  const data = await getMovieId();
+  data.movie_results.forEach(item => {
+    list += `<p class="top__movie--item" >${item.title}, Id: ${item.id}  </p> `;
+    listItem.innerHTML = list;
+  });
+};
+
+getMoviesById();
+
+const getMoviesTopRated = async function () {
+  let list = ``;
+  const listItem = document.querySelector(".top__rated");
+  const data = await getMovieTopRated();
+  data.results.slice(0, 10).forEach(item => {
+    list += `<li class="top__rated--item" >${item.title}, Id: ${item.id}  </li> `;
+    listItem.innerHTML = list;
+  });
+};
+
+getMoviesTopRated();
+
+const getMoviesByYear = async function () {
+  let list = ``;
+  const listItem = document.querySelector(".movies__1975");
+  const data = await getMovieByYear();
+  data.results.slice(0, 10).forEach(item => {
+    list += `<li class="movies__1975--li" >${item.title}, Id: ${item.id}  </li> `;
+    listItem.innerHTML = list;
+  });
+};
+
+getMoviesByYear();
