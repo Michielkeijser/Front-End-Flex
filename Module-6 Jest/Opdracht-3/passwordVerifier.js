3c201aadd67f03266d203c2b1ceb38246041b946
Michiel Keijser <chielkeijser@gmail.com>
2020-09-16 16:23:48 +0200

    Module-6 Opdracht-3

--- Module-6 Jest/Opdracht-3/passwordVerifier.js
+++ Module-6 Jest/Opdracht-3/passwordVerifier.js
@@ -4,17 +4,17 @@ const hasUppercase = pw => /[A-Z]/.test(pw);
 const isLessThen8Chars = pw => pw.length <= 8;
 const isNotBlank = pw => pw !== null;
 
-const passwordVerifier = password => {
+const passwordVerifier = pw => {
   const verifier = [
-    hasDigit(password),
-    hasLowercase(password),
-    hasUppercase(password),
-    isLessThen8Chars(password),
-    isNotBlank(password),
+    hasDigit(pw),
+    hasLowercase(pw),
+    hasUppercase(pw),
+    isLessThen8Chars(pw),
+    isNotBlank(pw),
   ];
   const conditionsTrue = verifier.filter(Boolean).length;
   console.log(conditionsTrue);
-  if (conditionsTrue >= 3 && hasLowercase(password)) {
+  if (conditionsTrue >= 3 && hasLowercase(pw)) {
     return true;
   } else {
     return false;
--- Module-6 Jest/Opdracht-3/passwordVerifier.test.js
+++ Module-6 Jest/Opdracht-3/passwordVerifier.test.js
@@ -1,9 +1,28 @@
 const { passwordVerifier } = require("./passwordVerifier.js");
 
 test("password verifier voldoet aan alle eisen", () => {
-  expect(passwordVerifier("a1Sfg")).toBe(true);
+  expect(passwordVerifier("henkie1")).toBe(true);
 });
 
-test("password verifier voldoet niet aan alle eisen", () => {
-  expect(passwordVerifier("AAAA123")).toBe(false);
+test("password verifier voldoet aan alle eisen", () => {
+  expect(passwordVerifier("1234a")).toBe(true);
+});
+
+test("password verifier voldoet aan alle eisen", () => {
+  expect(passwordVerifier("z")).toBe(true);
+});
+test("password verifier voldoet aan alle eisen", () => {
+  expect(passwordVerifier("henkie1234")).toBe(true);
+});
+test("password verifier voldoet aan alle eisen", () => {
+  expect(passwordVerifier("HENKhenk")).toBe(true);
+});
+test("password verifier voldoet aan alle eisen", () => {
+  expect(passwordVerifier("HENK33$")).toBe(false);
+});
+test("password verifier voldoet aan alle eisen", () => {
+  expect(passwordVerifier("1234")).toBe(false);
+});
+test("password verifier voldoet aan alle eisen", () => {
+  expect(passwordVerifier("")).toBe(false);
 });
