307cb70c046ec8cb5a8d8ddc83facae3317dff0c
Michiel Keijser <chielkeijser@gmail.com>
2020-10-28 11:09:34 +0100

    joe

--- Module-7 React/my-groceries-app/src/components/ShoppingList.jsx
+++ Module-7 React/my-groceries-app/src/components/ShoppingList.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import InputForm from './InputForm'
 import List from './List'
-import './ShoppingList.css'
 
 export default class ShoppingList extends Component {
     render() {
